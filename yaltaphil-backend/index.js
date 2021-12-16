b14dd5037b9ea9011614d763bbda921e1c9fe254
Yaltaphil <yaltaphil@yandex.ru>
2021-12-16 10:15:40 +0300

    mongodb connected

--- yaltaphil-backend/index.js
+++ yaltaphil-backend/index.js
@@ -1,50 +1,48 @@
-// const express = require("express");
+const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-// const app = express();
-// const port = 3000;
+const app = express();
+const port = 3000;
+
 const { MongoClient } = require("mongodb");
+const mongo = new MongoClient(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-async function insertOne() {
-    let client;
-    try {
-        client = new MongoClient(process.env.CONNECTIONSTRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            connectTimeoutMS: 3000,
-            keepAlive: 1,
+app.get("/", (req, res) => {
+    res.send(`<h1>Hello</h1><a href="/users">Users</a>`);
+});
+
+app.get("/users", (req, res) => {
+    connect();
+    res.send(
+        `<h1>This is my server users page</h1>
+        <p>And I'd like to describe all users here: </p>
+        <a href="/exit">exit</a>`
+    );
+});
+app.get("/exit", (req, res) => {
+    res.send(`<button>EXIT</button>`);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+function connect() {
+    mongo.connect((err) => {
+        console.log("MongoDB connected");
+        collection = mongo.db("site-db").collection("users");
+        for (let i = 0; i < 11; i++) {
+            collection.insertOne({ name: `Player ${i}` });
+        }
+        collection.find({ name: "Philip" }).toArray((err, result) => {
+            console.log(result);
         });
-        client.connect(async (err) => {
-            const collection = client.db("site-db").collection("posts");
-            collection.insertOne({ item: "card", qty: 115 });
-            // console.log( await collection.find().toArray());
-        });
-    } catch (error) {
-        console.log(error);
-    } finally {
-        client.close();
-    }
+
+        collection.deleteMany({ name: /^Player/ }, (err, result) =>
+            console.log(result)
+        );
+    });
 }
-
-insertOne();
-// app.get("/", (req, res) => {
-//     res.send(`<h1>Hello pusya</h1><a href="/users">Users</a>`);
-// });
-
-// app.get("/users", (req, res) => {
-//     client.connect((err) => {
-//         db = client.db("testdb");
-//         console.log("connected to mongo");
-//         db.collection("users").insertOne({ a: 1 }, function (err, r) {});
-
-//         client.close();
-
-//         res.send(
-//             `<h1>This is my server users page</h1><p>And I'd like to describe all users here: </p>`
-//         );
-//     });
-// });
-
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`);
-// });
