217f2db27f01fed70735572dcdde85bd32f1c880
Yaltaphil <yaltaphil@yandex.ru>
2021-12-18 21:51:30 +0300

    round 2

--- yaltaphil-backend/index.js
+++ yaltaphil-backend/index.js
@@ -1,8 +1,25 @@
+const cors = require("cors");
 const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config();
+
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+
 const app = express();
-const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json());
+
+
+// app.set("view engine", "html");
+
+// // Static folder
+// app.use(express.static(__dirname + "/views/"));
+
+// // Defining route middleware
+// app.use("/api", require("./routes/api"));
+
+
 
 const { MongoClient } = require("mongodb");
 const mongo = new MongoClient(process.env.MONGO_URI, {
@@ -10,39 +27,40 @@ const mongo = new MongoClient(process.env.MONGO_URI, {
     useUnifiedTopology: true,
 });
 
-app.get("/", (req, res) => {
-    res.send(`<h1>Hello</h1><a href="/users">Users</a>`);
-});
+let user;
 
-app.get("/users", (req, res) => {
-    connect();
-    res.send(
-        `<h1>This is my server users page</h1>
-        <p>And I'd like to describe all users here: </p>
+mongo.connect((err) => {
+    app.get("/", async (req, res) => {
+        // for (let i = 0; i < 10000; i++) {
+        //     mongo
+        //         .db("site-db")
+        //         .collection("users")
+        //         .insertOne({ name: `User-${i}`, role: "user2" });
+        // }
+
+        res.send(
+            `<h1>This is my server users page</h1>
+       <p>And I'd like to describe all users here: </p>
+       <form action="/result" method="POST">
+       <input type="text" name="name"/>
+       <button>Submit</button>
+       </form>
         <a href="/exit">exit</a>`
-    );
-});
-app.get("/exit", (req, res) => {
-    res.send(`<button>EXIT</button>`);
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
-
-function connect() {
-    mongo.connect((err) => {
-        console.log("MongoDB connected");
-        collection = mongo.db("site-db").collection("users");
-        for (let i = 0; i < 11; i++) {
-            collection.insertOne({ name: `Player ${i}` });
-        }
-        collection.find({ name: "Philip" }).toArray((err, result) => {
-            console.log(result);
-        });
-
-        collection.deleteMany({ name: /^Player/ }, (err, result) =>
-            console.log(result)
         );
     });
-}
+    app.post("/result", (req, res) => {
+        console.log(req.body.name);
+        mongo
+            .db("site-db")
+            .collection("users")
+            .find({ name: req.body.name })
+            .toArray()
+            .then((result) => result.forEach((item) => console.log(item.role)));
+
+        res.send(`We got ${user}`);
+    });
+
+    app.listen(port, () => {
+        console.log(`listening at http://localhost:${port}`);
+    });
+});
