313639f70525f226c34197514866707fc3349989
Yaltaphil <yaltaphil@yandex.ru>
2021-12-25 21:26:20 +0300

    front initial

---
+++ yaltaphil-frontend/src/App.vue
@@ -0,0 +1,194 @@
+<script setup>
+    import portfolioItem from "./components/portfolio-item.vue";
+    const sertificates = [
+        {
+            title: "DOIT gaming portal",
+            picture: "/src/assets/sertificates/4.png",
+            link: "https://doit.yaltaphil.ru",
+        },
+        {
+            title: "Porten landing",
+            picture: "/src/assets/sertificates/4.png",
+            link: "https://doit.yaltaphil.ru",
+        },
+        {
+            title: "Porten landing",
+            picture: "/src/assets/sertificates/4.png",
+            link: "https://doit.yaltaphil.ru",
+        },
+        {
+            title: "Porten landing",
+            picture: "/src/assets/sertificates/4.png",
+            link: "https://doit.yaltaphil.ru",
+        },
+        {
+            title: "Porten landing",
+            picture: "/src/assets/sertificates/4.png",
+            link: "https://doit.yaltaphil.ru",
+        },
+        {
+            title: "Porten landing",
+            picture: "/src/assets/sertificates/4.png",
+            link: "https://doit.yaltaphil.ru",
+        },
+        {
+            title: "Porten landing",
+            picture: "/src/assets/sertificates/4.png",
+            link: "https://doit.yaltaphil.ru",
+        },
+    ];
+</script>
+
+<template>
+    <div class="min-w-full min-h-screen">
+        <div class="max-w-screen-xl bg-gray-100 mx-auto">
+            <nav class="bg-white shadow dark:bg-gray-800">
+                <div class="container px-6 py-3 mx-auto md:flex">
+                    <div class="flex items-center justify-between">
+                        <img
+                            class="w-32 h-32"
+                            alt="Vue logo"
+                            src="./assets/y-logo.svg"
+                        />
+
+                        <!-- Mobile menu button -->
+                        <div class="flex md:hidden">
+                            <button
+                                type="button"
+                                class="
+                                    text-gray-500
+                                    dark:text-gray-200
+                                    hover:text-gray-600
+                                    dark:hover:text-gray-400
+                                    focus:outline-none focus:text-gray-600
+                                    dark:focus:text-gray-400
+                                "
+                                aria-label="toggle menu"
+                            >
+                                <svg
+                                    viewBox="0 0 24 24"
+                                    class="w-6 h-6 fill-current"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                                    ></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+
+                    <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+                    <div
+                        class="
+                            w-full
+                            md:flex md:items-center md:justify-between
+                        "
+                    >
+                        <div
+                            class="
+                                flex flex-col
+                                px-2
+                                py-3
+                                -mx-4
+                                md:flex-row md:mx-0 md:py-0
+                            "
+                        >
+                            <a
+                                href="#"
+                                class="
+                                    px-2
+                                    py-1
+                                    text-sm
+                                    font-medium
+                                    text-gray-700
+                                    transition-colors
+                                    duration-200
+                                    transform
+                                    rounded
+                                    dark:text-gray-200
+                                    hover:bg-gray-900 hover:text-gray-100
+                                    md:mx-2
+                                "
+                                >Start</a
+                            >
+                            <a
+                                href="#"
+                                class="
+                                    px-2
+                                    py-1
+                                    text-sm
+                                    font-medium
+                                    text-gray-700
+                                    transition-colors
+                                    duration-200
+                                    transform
+                                    rounded
+                                    dark:text-gray-200
+                                    hover:bg-gray-900 hover:text-gray-100
+                                    md:mx-2
+                                "
+                                >About me</a
+                            >
+                            <a
+                                href="#"
+                                class="
+                                    px-2
+                                    py-1
+                                    text-sm
+                                    font-medium
+                                    text-gray-700
+                                    transition-colors
+                                    duration-200
+                                    transform
+                                    rounded
+                                    dark:text-gray-200
+                                    hover:bg-gray-900 hover:text-gray-100
+                                    md:mx-2
+                                "
+                                >Portfolio</a
+                            >
+                            <a
+                                href="#"
+                                class="
+                                    px-2
+                                    py-1
+                                    text-sm
+                                    font-medium
+                                    text-gray-700
+                                    transition-colors
+                                    duration-200
+                                    transform
+                                    rounded
+                                    dark:text-gray-200
+                                    hover:bg-gray-900 hover:text-gray-100
+                                    md:mx-2
+                                "
+                                >Contact</a
+                            >
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <div
+                class="
+                    flex flex-wrap
+                    justify-center
+                    align-around
+                    bg-blue-100
+                    gap-16
+                    py-16
+                "
+            >
+                <portfolio-item
+                    v-for="(item, i) in sertificates"
+                    :key="i"
+                    :project="item.title"
+                    :link="item.link"
+                    :picture="item.picture"
+                />
+            </div>
+        </div>
+    </div>
+</template>
---
+++ yaltaphil-frontend/src/components/portfolio-item.vue
@@ -0,0 +1,115 @@
+<script setup>
+    import { ref } from "vue";
+
+    defineProps({
+        project: String,
+        link: String,
+        picture: String,
+    });
+
+    const count = ref(0);
+</script>
+
+<template>
+    <a :href="link" target="_blank"
+        class="
+            w-64
+            overflow-hidden
+            bg-sky-100
+            rounded-lg
+            shadow-2xl
+            hover:cursor-pointer hover:drop-shadow-2xl
+            transition transition-colors
+            ease-in-out
+            duration-500
+        "
+    >
+        <img
+            class="
+                object-cover object-center
+                w-full
+                h-1/2
+                border-2 border-stone-300
+                ring-1
+                grayscale
+                hover:grayscale-0
+                hover:saturate-150
+                transition transition-colors
+                ease-in-out
+                duration-500
+            "
+            :src="picture"
+            alt="avatar"
+        />
+
+        <div
+            class="
+                h-1/6
+                p-2
+                flex
+                items-center
+                bg-gradient-to-b
+                from-blue-400
+                to-teal-200
+            "
+        >
+            <svg
+                class="w-6 h-6 text-white fill-red-700"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M17 21C15.8954 21 15 20.1046 15 19V15C15 13.8954 15.8954 13 17 13H19V12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12V13H7C8.10457 13 9 13.8954 9 15V19C9 20.1046 8.10457 21 7 21H3V12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12V21H17ZM19 15H17V19H19V15ZM7 15H5V19H7V15Z"
+                />
+            </svg>
+
+            <h1 class="mx-3 text-lg font-bold text-gray-800 font-sans">
+                {{ project }}
+            </h1>
+        </div>
+
+        <div
+            class="
+                h-2/6
+                px-6
+                py-4
+                hover:contrast-75
+                transition transition-colors
+                ease-in-out
+                duration-500
+            "
+        >
+            <h2 class="text-xl font-semibold text-gray-800 dark:text-white">
+                Philip
+            </h2>
+
+            <p class="py-2 text-gray-700 dark:text-gray-400">
+                Full Stack software engineer. Love javascript technologies
+                stack.
+            </p>
+
+            <div
+                class="flex items-center mt-4 text-gray-700 dark:text-gray-200"
+            >
+                <svg
+                    class="w-6 h-6 fill-current"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="M14 11H10V13H14V11Z" />
+                    <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M7 5V4C7 2.89545 7.89539 2 9 2H15C16.1046 2 17 2.89545 17 4V5H20C21.6569 5 23 6.34314 23 8V18C23 19.6569 21.6569 21 20 21H4C2.34314 21 1 19.6569 1 18V8C1 6.34314 2.34314 5 4 5H7ZM9 4H15V5H9V4ZM4 7C3.44775 7 3 7.44769 3 8V14H21V8C21 7.44769 20.5522 7 20 7H4ZM3 18V16H21V18C21 18.5523 20.5522 19 20 19H4C3.44775 19 3 18.5523 3 18Z"
+                    />
+                </svg>
+
+                <h2 class="px-2 text-sm">{{ link }}</h2>
+            </div>
+        </div>
+    </a>
+</template>
