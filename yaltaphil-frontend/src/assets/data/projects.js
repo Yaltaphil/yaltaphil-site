06020f54ed41cbb9e79b1f94c48e3858e664ec46
Yaltaphil <yaltaphil@yandex.ru>
2022-01-11 20:34:29 +0300

    feature: descending project order

--- yaltaphil-frontend/src/assets/data/projects.js
+++ yaltaphil-frontend/src/assets/data/projects.js
@@ -4,20 +4,20 @@ export const projects = [{
     picture: "/img/portfolio/doit1.jpg",
     link: "https://doit.yaltaphil.ru",
 }, {
-    title: "Porten landing",
-    description: "Landing page for luxury watches shop. Pure html, js and css.",
-    picture: "/img/portfolio/porten.jpg",
-    link: "https://yaltaphil.github.io/porten/",
+    title: "Kino-CMS",
+    description: "Cinema site and it's admin panel. Written on Vue 2 and bootstrap admin-lte. Google Firebase used as a backend.",
+    picture: "/img/portfolio/kinocms.jpg",
+    link: "https://kinocms.yaltaphil.ru",
 }, {
     title: "Big Theater landing",
     description: "Landing page for big theather site. Scss used",
     picture: "/img/portfolio/big-theater.gif",
     link: "https://yaltaphil.github.io/big-theater/",
 }, {
-    title: "Kino-CMS",
-    description: "Cinema site and it's admin panel. Written on Vue 2 and bootstrap admin-lte. Google Firebase used as a backend.",
-    picture: "/img/portfolio/kinocms.jpg",
-    link: "https://kinocms.yaltaphil.ru",
+    title: "Porten landing",
+    description: "Landing page for luxury watches shop. Pure html, js and css.",
+    picture: "/img/portfolio/porten.jpg",
+    link: "https://yaltaphil.github.io/porten/",
 }, {
     title: "KPK Status",
     description: "Site for credit union. Written on Vue 2 and Vuetify library.",
